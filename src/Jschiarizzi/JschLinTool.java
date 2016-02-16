f768032b235e65cfd0401bf177087b80116b7e97
BuildTools <unconfigured@null.spigotmc.org>
2016-02-15 21:37:35 -0500

    Completed code, final test won't work though.  Could be a serious bug that I'm missing.  Most likely in the last 4 methods of JschLinTool.java.

--- src/Jschiarizzi/JschLinTool.java
+++ src/Jschiarizzi/JschLinTool.java
@@ -30,8 +30,6 @@ public class JschLinTool extends LinToolEmpty {
 
     public  double norm (double[] v){
 
-        //TODO fix this shit
-
         double total = 0;
 
         if(v == null){
@@ -70,7 +68,26 @@ public class JschLinTool extends LinToolEmpty {
 
     public  boolean approxEquals (double[] u, double[] v, double errorTolerance){
 
-        return false;
+        if(u.length != v.length || u == null || v == null){
+            return false;
+        }
+
+        double negTol;
+        //easier to check a little above and a little below zero with this variable
+        if(errorTolerance < 0){
+            errorTolerance *= -1;//make it positive
+        }
+        negTol = 0 - errorTolerance;
+
+        double total = 0;
+        for(int i = 0; i < u.length; i++){
+            total = (u[i]*u[i]) - (v[i]*v[i]);
+            if(total > errorTolerance || total < negTol){
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public  double cosine (double[] u, double[] v){
@@ -81,27 +98,95 @@ public class JschLinTool extends LinToolEmpty {
     // Matrix operations:
     public  double[][] add (double[][] A, double[][] B){
 
-        return A;
+        if(A == null || B == null || A[0].length != B[0].length || A.length != B.length){
+            return null;
+        }
+
+        double[][] C = new double[A.length][A[0].length];
+
+        for(int i = 0; i < A.length; i++){
+            for(int j = 0; j < A[i].length; j++){
+                C[i][j] = A[i][j] + B[i][j];
+            }
+        }
+
+
+        return C;
     }
 
     public  double[][] scalarProduct (double alpha, double[][] A){
+
+        if(A == null){
+            return null;
+        }
+
+        for(int i = 0; i < A.length; i++){
+            for(int j = 0; j < A[i].length; j++){
+                A[i][j] = alpha * A[i][j];
+            }
+        }
 
         return A;
     }
 
     public  double[][] mult (double[][] A, double[][] B){
 
-        return A;
+        if(A == null || B == null || A[0].length != B.length){
+            return null;
+        }
+
+        double[][] C = new double[A.length][B[0].length];
+        //System.out.println("C is " + A.length + " by " + B[0].length);
+
+
+        for(int i = 0; i < A.length; i++){
+            for(int j = 0; j < B[i].length; j++){
+
+                for (int k = 0; k < A[0].length; k++ ){
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+
+            }//end j
+        }//end i
+
+        return C;
     }
 
     public  double[] matrixVectorMult (double[][] A, double[] v){
 
-        return v;
+        if(A == null || v == null || A[0].length != v.length){
+            return null;
+        }
+
+        double[] C = new double [A.length];
+
+        for (int i = 0; i< A.length; i++){
+            for (int j = 0; j < A[i].length; j++){
+                C[i] += v[j] * A[i][j];
+            }
+        }
+
+        return C;
     }
 
     public  double[] vectorMatrixMult (double[] v, double[][] A){
 
-        return v;
+        if(v == null || A == null){
+            return null;
+        }else if(v.length != A.length){
+            return null;
+        }
+
+        double[] C = new double[A[0].length];
+
+        for(int i = 0; i < A[0].length; i++){
+            for (int j = 0; j < A.length; j++){
+                //do like previous method except kinda backwards.
+                C[i] += A[j][i] * v[j];
+            }
+        }
+
+        return C;
     }
 
     public  double[][] vectorLeftMult (double[] u, double[] v){
@@ -111,26 +196,94 @@ public class JschLinTool extends LinToolEmpty {
 
     public  double[][] transpose (double[][] A){
 
-        return A;
+        if(A == null){
+            return null;
+        }
+
+        double[][] C = new double[A[0].length][A.length];
+
+        for (int i = 0; i < A.length; i++)
+            for (int j = 0; j < A[i].length; j++)
+                C[j][i] = A[i][j];
+        return C;
     }
 
     public  double frobeniusNorm (double[][] A){
 
-        return 0;
+        if(A == null){
+            return -1;
+        }
+
+        double norm = 0;
+
+        for(int i = 0; i < A.length; i++){
+            for(int j = 0; j < A[i].length; j++){
+                norm += A[i][j] * A[i][j];
+            }
+        }
+
+        norm = Math.sqrt(norm);
+
+        return norm;
     }
 
     public  boolean approxEquals (double[][] A, double[][] B, double errorTolerance){
 
-        return false;
+        if(A == null || B == null){
+            return false;
+        }
+
+        if(A.length != B.length || A[0].length != B[0].length){
+            return false;
+        }
+
+        if(errorTolerance < 0){
+            errorTolerance *= -1;
+        }
+        double negErrTol = 0 - errorTolerance;
+
+
+        double total = 0;
+        for(int i = 0; i < A.length; i++){
+            for(int j = 0; j < A[0].length; j++){
+                total = (A[i][j]*A[i][j]) - (B[i][j]*B[i][j]);
+
+                if(total > errorTolerance || total < negErrTol){
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     public  double[] getColumnAsVector (double[][] A, int col){
 
-        return new double[0];
+        if(A == null){
+            return null;
+        }
+
+        double[] C = new double[A[0].length];
+
+        for(int i = 0; i < A[0].length; i++){
+            C[i] = A[i][col];
+        }
+
+        return C;
     }
 
     public  double[] getRowAsVector (double[][] A, int row){
 
-        return new double[0];
+        if(A == null){
+            return null;
+        }
+
+        double[] C = new double[A[0].length];
+
+        for(int i = 0; i < A[0].length; i++){
+            C[i] = A[row][i];
+        }
+
+        return C;
     }
 }
--- src/Jschiarizzi/TestA1.java
+++ src/Jschiarizzi/TestA1.java
@@ -1,6 +1,6 @@
 package Jschiarizzi;
 
-import edu.gwu.lintool.*;
+import edu.gwu.lintool.LinTest;
 
 public class TestA1 {
 
@@ -8,18 +8,19 @@ public class TestA1 {
     {
 	// Make an instance of your ComplexNumber class. REPLACE
 	// AliceComplex below with your version.
-	//JschComplex c = new JschComplex (3, 5);
-    //c.angle();
+	JschComplex c = new JschComplex (3, 5);
+    c.angle();
 	// This tests your implementation of complex numbers.
-	//LinTest.testComplex (c);
+	LinTest.testComplex (c);
 
 	// REPLACE the line below with your tool, and un-comment.
 	JschLinTool lin = new JschLinTool();
 
 	// Un-comment tests one by one until all are passed.
 	LinTest.testVectorOperations (lin);
-	//LinTest.testMatrixOperations (lin);
-	//LinTest.testComplexVectors (lin);
+	LinTest.testMatrixOperations (lin);
+	LinTest.testComplexVectors (lin);
+
     }
 
 }
